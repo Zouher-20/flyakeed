c5e0f932b23ae6ef6e6eaabe05dcdbf6a2d2555f
Zouhair Nasser <zouhair.nasser@workiom.com>
2024-03-28 23:32:46 +0300

    first commit

---
+++ src/views/ResultsView.vue
@@ -0,0 +1,4 @@
+<template>
+results page
+</template>
+
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#196dfb",
+        "primary-dark": "#1a1f71",
+        "primary-text": "#114aaf",
+        "primary-darker": "#0a0f57",
+        "gray-hove": "#e6e6e6",
+        "gray-soft": "#f1f1f1",
+        gray: "#8f8f8f",
+        "gray-dark": "#585858",
+        secondary: "#ff56a1",
+      },
+    },
+  },
+  plugins: [],
+};
